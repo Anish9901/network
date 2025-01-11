fdd0a2129974f015e87e874eafe09ae40ec3edfd
Anish Umale <umaleanish120@gmail.com>
2025-01-12 02:01:47 +0530

    add post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    username = models.ForeignKey('User', on_delete=models.CASCADE)
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
